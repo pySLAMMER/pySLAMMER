bd60a12859f65be50bc38eb5dcd76ac484cc4aec
Donald Garcia-Rivas <rivas203@uw.edu>
2024-08-21 17:59:35 -0700

    Added plotting method.

--- pyslammer/Record.py
+++ pyslammer/Record.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import scipy.integrate as spint
 
 G_EARTH = 9.80665 # Acceleration due to gravity (m/s^2).
@@ -31,3 +32,48 @@ class Record():
                     'PGA   : {:.3f} g\n'.format(self.pga)+
                     'dt    : {:.3f} s'.format(self.dt))
         return info
+    
+    def plot(self, acc=True, vel=True, disp=True):
+        if self.dt == -1.0:
+            return
+        num_plots = sum([acc, vel, disp])
+        remain_plots = num_plots
+        if num_plots == 1:
+            fig, ax = plt.subplots(figsize=(10, 10))
+        else:
+            fig, ax = plt.subplots(num_plots, 1, figsize=(10, 10))
+        if acc:
+            if num_plots == 1:
+                acc = ax
+            else:
+                i = num_plots - remain_plots
+                remain_plots -= 1
+                acc = ax[i]
+            acc.plot(self.time, self.gnd_acc, label='Ground Acceleration')
+            acc.set_ylabel('Acceleration (m/s^2)')
+            acc.set_title('Ground Acceleration')
+            acc.legend()
+        if vel:
+            if num_plots == 1:
+                vel = ax
+            else:
+                j = num_plots - remain_plots
+                remain_plots -= 1
+                vel = ax[j]
+            vel.plot(self.time, self.gnd_vel, label='Ground Velocity')
+            vel.set_ylabel('Velocity (m/s)')
+            vel.set_title('Ground Velocity')
+            vel.legend()
+        if disp:
+            if num_plots == 1:
+                disp = ax
+            else:
+                k = num_plots - remain_plots
+                remain_plots -= 1
+                disp = ax[k]
+            disp.plot(self.time, self.gnd_disp, label='Ground Displacement')
+            disp.set_xlabel('Time (s)')
+            disp.set_ylabel('Displacement (m)')
+            disp.set_title('Ground Displacement')
+            disp.legend()
+        plt.show()
