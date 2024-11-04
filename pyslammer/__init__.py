de28b16c27a35a92a6e9a52eaa5f004ca0856b64
Lorne <lornearnold@gmail.com>
2024-11-04 15:45:59 -0800

    add dynamic version to toml

--- pyslammer/__init__.py
+++ pyslammer/__init__.py
@@ -7,7 +7,7 @@ from pyslammer.utilities import *
 # import pyslammer.analytical_sliding_block as asb
 import pyslammer.demo_widgets as dw
 
-__version__ = "0.1.17"
+__version__ = "0.1.18"
 
 import os
 
